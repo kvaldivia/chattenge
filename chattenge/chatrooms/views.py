92ffc9176023688df9fe451b9763c48c00f4640f
Kenny Valdivia <k_valdivia@gmx.com>
2020-09-27 18:37:10 -0500

    Javascript modules for chatrooms

--- chattenge/chatrooms/views.py
+++ chattenge/chatrooms/views.py
@@ -4,7 +4,6 @@ from django.contrib.auth.mixins import LoginRequiredMixin
 from django.urls import reverse
 from django.utils.translation import gettext_lazy as _
 from django.views.generic import DetailView, ListView, RedirectView, UpdateView
-from django.core import HttpResponse
 
 from chattenge.chatrooms.models import Chatroom
 
--- chattenge/static/js/project.js
+++ chattenge/static/js/project.js
@@ -1,4 +1,72 @@
-/* Project specific Javascript goes here. */
-const redirect = (url) {
-  window.open(url);
-}
+$(document).ready(() => {
+
+  const chatrooms = (utils) => {
+    /*
+     * Perform a post request to send a message in the chatroom.
+    **/
+    const postMessageToChatroom = (chatroomId, content) => {
+      return utils.post(`/chatroom/${chatroomId}/message`, { content });
+    }
+
+    /*
+     * Perform a post request to include the user as a member in the chatroom.
+    **/
+    const joinChatroom = (chatroomId) => {
+      return utils.post(`/chatroom/${chatroomId}/join`, {});
+    }
+
+    /*
+     * Perform a post request to include the user as a member in the chatroom.
+    **/
+    const leaveChatroom = (chatroomId) => {
+      return utils.post(`/chatroom/${chatroomId}/leave`, {});
+    }
+
+    return {
+      postMessageToChatroom,
+      joinChatroom,
+      leaveChatroom,
+    };
+  }
+
+  const utils = () => {
+    const DEFAULT_HEADERS = { 'Content-Type': 'application/json' }
+
+    const post = async (url, headers, body) => {
+      const response = await fetch(url, {
+        method: 'POST',
+        headers: {
+          ...DEFAULT_HEADERS,
+          ...headers
+        },
+        ...body
+      });
+      return response.json();
+    }
+
+    const get = async (url, headers) => {
+      const response = await fetch(url, {
+        method: 'GET',
+        headers: {
+          ...DEFAULT_HEADERS,
+          ...headers
+        }
+      });
+      return response.json();
+    }
+     
+    return {
+      post,
+      get
+    }
+  }
+
+  const _utils = utils();
+
+  const exports = {
+    utils: _utils,
+    chatrooms: chatrooms(_utils),
+  }
+
+  window.chattenge = exports;
+});
