ce73b67af12d3607859a6b26a18e0238cf9e8c73
Kenny Valdivia <k_valdivia@gmx.com>
2020-09-27 21:55:32 -0500

    Message model

--- chattenge/chatrooms/models.py
+++ chattenge/chatrooms/models.py
@@ -8,3 +8,11 @@ class Chatroom(models.Model):
     name = models.CharField(max_length=20, default="")
     description = models.CharField(max_length=200, default="")
     members = models.ManyToManyField(User)
+
+
+class Message(models.Model):
+
+    author = models.ForeignKey(User, on_delete=models.DO_NOTHING)
+    chatroom = models.ForeignKey(Chatroom, on_delete=models.DO_NOTHING)
+    content = models.TextField(null=False)
+    sent = models.DateTimeField()
